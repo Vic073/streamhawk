253794268123308e56691dc95684f8e23319ae6f
Victor Chilomo <chilomovictor@gmail.com>
2026-04-29 12:31:37 +0200

    ...adding web interfaces

--- streamhawk/__init__.py
+++ streamhawk/__init__.py
@@ -8,17 +8,36 @@ __version__ = "2.0.0"
 __author__ = "Project Horizon"
 
 from .config import Config
-from .extractor import StreamExtractor
-from .downloader import YTDLPManager
-from .imdb import IMDbClient
-from .utils import logger
+from .extractor import StreamExtractor, StreamInfo
+from .downloader import YTDLPManager, DownloadProgress
+from .imdb import IMDbClient, MovieMetadata
+from .browser import StealthBrowser, RequestInterceptor
+from .m3u8_parser import M3U8Parser, M3U8Stream
+from .subtitles import SubtitleManager, Subtitle
+from .postprocessor import PostProcessor, ProcessingOptions
+from .fingerprints import FingerprintManager, StealthHelper
 from .notifications import NotificationManager
+from .utils import logger, HistoryManager
 
 __all__ = [
     "Config",
-    "StreamExtractor", 
+    "StreamExtractor",
+    "StreamInfo",
     "YTDLPManager",
+    "DownloadProgress",
     "IMDbClient",
-    "logger",
+    "MovieMetadata",
+    "StealthBrowser",
+    "RequestInterceptor",
+    "M3U8Parser",
+    "M3U8Stream",
+    "SubtitleManager",
+    "Subtitle",
+    "PostProcessor",
+    "ProcessingOptions",
+    "FingerprintManager",
+    "StealthHelper",
     "NotificationManager",
+    "logger",
+    "HistoryManager",
 ]
